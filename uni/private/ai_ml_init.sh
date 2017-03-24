80563cda0337cac6a4331834f8ac9749dcfdfba5
M.R.Z <GdZ@users.noreply.github.com>
2017-03-24 17:40:45 +0100

    Create ai_ml_init.sh
    add new module for machine learning

---
+++ uni/private/ai_ml_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+WORK_DIR=${HOME}/works/uni/ai
+LIB_DIR=${WORK_DIR}/lib
+
+## cafe dor bvml
+CAFE_URL="https://github.com/BVLC/caffe.git";
+
+## cvprw baesd on cafe
+CVPRW_BASED_CAFE="git@github.com:GdZ/caffe-cvprw15.git";
+
+## clone lib modules
+cd ${LIB_DIR}
+### clone cafe
+git clone ${CAFE_URL} .
+### clone cvprw
+git clone ${CVPRW_BASED_CAFE} .
+
